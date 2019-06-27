572ddab37ce88e2bdedbd1dddd8cc27877f624a5
cjiasu <40857324+cjiasu@users.noreply.github.com>
2019-06-27 21:48:04 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#=================================================
+#	System Required:Debian 9
+#	Description: Lotserver
+#	Version: 1.0.0
+#	Author: 无尽
+# telegram: @WuJing
+#=================================================
+
 #下载内核：
 wget http://security-cdn.debian.org/pool/updates/main/l/linux/linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb
 
@@ -11,6 +19,11 @@ sleep 1
 #删除内核
 apt-get purge  linux-image-4.9.0-8-amd64 -y
 sleep 1
-echo -e "\033[41;36m  5s later will reboot your server  \033[0m";
+stty erase '^H' && read -p "需要重启VPS后，才能开启锐速内核加速，是否现在重启 ? [Y/n] :" yn
+	[ -z "${yn}" ] && yn="y"
+	if [[ $yn == [Yy] ]]; then
+		echo -e "${Info} VPS 重启中..."
+		reboot
+	fi
 
 
