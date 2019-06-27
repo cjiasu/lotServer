ee214c2a532dfd9a5f18852a5a90ea3118696ab4
cjiasu <40857324+cjiasu@users.noreply.github.com>
2019-06-27 22:47:40 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -17,7 +17,7 @@ dpkg -i linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb
 
 sleep 1
 #删除内核
-apt-get purge  linux-image-4.9.0-8-amd64 -y
+apt-get purge  linux-image-4.9.0-9-amd64 -y
 sleep 1
 stty erase '^H' && read -p "需要重启VPS后，才能开启锐速内核加速，是否现在重启 ? [Y/n] :" yn
 	[ -z "${yn}" ] && yn="y"
