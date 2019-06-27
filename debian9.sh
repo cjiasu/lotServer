6a2e82e7bbdbdaa1341e9174cfa778be490e6c7b
cjiasu <40857324+cjiasu@users.noreply.github.com>
2019-06-27 21:39:28 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -10,6 +10,7 @@ dpkg -i linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb
 sleep 1
 #删除内核
 apt-get purge  linux-image-4.9.0-8-amd64 -y
+sleep 1
 echo -e "\033[41;36m  5s later will reboot your server  \033[0m";
-sleep 5
-reboot
+
+
